f75ffd6899b1320cd16f62c930b5595107deeca4
svaflica <tarskaya.alina@mail.ru>
2022-04-11 23:20:57 +0300

    changed index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 5000
+const IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.get('/', (req, res) => {
   res.send('<html><body>Hello World!</body></html>')
 })
 
-app.listen(port, () => {
+app.listen(port, IP, () => {
   console.log(`Example app listening on port ${port}`)
 })
